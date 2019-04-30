9cb6666009e7d21d0870ee68616c260b0d309e7c
Yaroslav Tatarenko <yaroslavtatarenko@gmail.com>
2019-04-30 12:47:04 +0300

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@
 
 export DOCKER_ID_USER="yartat"
 
-cat docker-deploy.yml \
+cat deploy.yml \
     | sed -e 's!$DOCKER_APP_IMAGE_ID!$DOCKER_ID_USER/prof_app!' \
     | sed -e 's!$DOCKER_NGINX_IMAGE_ID!$DOCKER_ID_USER/prof_nginx!' \
     | sed -e 's!$DOCKER_DB_IMAGE_ID!$DOCKER_ID_USER/prof_mysql!' \
     > docker-compose.yml
 
-#docker-compose -f deploy.v3.yml up -d
 docker-compose run -d --service-ports prof_nginx
